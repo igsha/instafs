cec967f6d1e7f3b49bd4d4fcef8ab780f4f07c22
Igor Sharonov <igsha@users.noreply.github.com>
2021-03-29 22:08:27 +0300

    Fix comment fetching

--- instafs/instagram.py
+++ instafs/instagram.py
@@ -1,9 +1,11 @@
 '''Implements instagram API'''
 
-import requests, re, json
+import requests
+import re, json, sys
 import urllib.parse
 from collections import namedtuple
 import datetime
+from requests.adapters import HTTPAdapter
 
 
 default_headers = {'User-agent': 'My user agent 1.0'}
@@ -20,7 +22,9 @@ class TokenManager(object):
         self.username = username
         url = f'https://www.instagram.com/{username}'
         base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urllib.parse.urlparse(url))
-        with requests.get(url, stream=False, allow_redirects=True, headers=default_headers) as r:
+        self.session = requests.Session()
+        self.session.mount("https://", HTTPAdapter(max_retries=5))
+        with self.session.get(url, stream=False, allow_redirects=True, headers=default_headers) as r:
             json_body = re.search('window._sharedData\s*=\s*({.*)(?=;</script>)', r.text)
             body = json.loads(json_body.group(1))
             self.user = body['entry_data']['ProfilePage'][0]['graphql']['user']
@@ -29,43 +33,49 @@ class TokenManager(object):
             script0 = re.search('href="([^"]+ProfilePageContainer.js[^"]+)', r.text)
             script1 = re.search('href="([^"]+Consumer.js[^"]+)', r.text)
             for script in script0, script1:
-                with requests.get(base_url + script.group(1), stream=True, allow_redirects=True, headers=default_headers) as r2:
+                with self.session.get(base_url + script.group(1), stream=True, allow_redirects=True, headers=default_headers) as r2:
                     result = re.search('c\.pagination\},queryId:"([^"]+)', r2.text)
                     if result is None:
                         continue
 
                     self.profile = result.group(1)
 
-            with requests.get(base_url + script1.group(1), stream=True, allow_redirects=True, headers=default_headers) as r2:
+            with self.session.get(base_url + script1.group(1), stream=True, allow_redirects=True, headers=default_headers) as r2:
                 regex = 'threadedComments\.parentByPostId\.get\(n\)\.pagination\},queryId:"([^"]+)'
                 self.comment = re.search(regex, r2.text).group(1)
 
 
 class Continuation(object):
-    def __init__(self, query_hash, id_dict, cursor, count=12):
+    def __init__(self, session, query_hash, id_dict, cursor, count=12):
         variables = json.dumps({**id_dict, 'first': count, 'after': cursor})
         base_url = 'https://www.instagram.com/graphql/query'
         self.url_ = f'{base_url}/?query_hash={query_hash}&variables={urllib.parse.quote(variables)}'
+        self.session = session
 
     def get(self):
-        return requests.get(self.url_, headers=default_headers).json()
+        try:
+            return self.session.get(self.url_, headers=default_headers).json()
+        except Exception as e:
+            print("Got " + str(e), file=sys.stderr)
+            raise
 
 
 class DataObject(object):
-    def __init__(self, url=None, content=None):
+    def __init__(self, session, url=None, content=None):
         self.url_ = url
         self.content_ = content
+        self.session = session
 
     def __getitem__(self, idx):
         if self.content_ is None:
-            with requests.get(self.url_, stream=True, headers=default_headers) as r:
+            with self.session.get(self.url_, stream=True, headers=default_headers) as r:
                 self.content_ = r.raw.read()
 
         return self.content_[idx]
 
     def __len__(self):
         if self.content_ is None:
-            with requests.head(self.url_, timeout=30, headers=default_headers) as r:
+            with self.session.head(self.url_, timeout=30, headers=default_headers) as r:
                 return int(r.headers['Content-Length'])
 
         return len(self.content_)
@@ -74,13 +84,13 @@ class DataObject(object):
 class Comment(object):
     CommentBody = namedtuple('CommentBody', 'text user time')
 
-    def __init__(self, cmt, shortcode, token):
+    def __init__(self, session, cmt, shortcode, token):
         self.count = cmt['count']
         self.token = token
         self.shortcode = shortcode
         if cmt['page_info']['has_next_page']:
             end_cursor = cmt['page_info']['end_cursor']
-            self.pagination = Continuation(token, {'shortcode': shortcode}, end_cursor, count=50)
+            self.pagination = Continuation(session, token, {'shortcode': shortcode}, end_cursor, count=50)
         else:
             self.pagination = None
 
@@ -107,30 +117,34 @@ class Comment(object):
         return self.pagination is not None
 
     def get_next(self):
-        data = self.pagination.get()
-        cmt = data['data']['shortcode_media']['edge_media_to_parent_comment']
-        if cmt['page_info']['has_next_page']:
-            end_cursor = cmt['page_info']['end_cursor']
-            self.pagination = Continuation(self.token, {'shortcode': self.shortcode}, end_cursor, count=50)
-        else:
-            self.pagination = None
+        try:
+            data = self.pagination.get()
+            cmt = data['data']['shortcode_media']['edge_media_to_parent_comment']
+            if cmt['page_info']['has_next_page']:
+                end_cursor = cmt['page_info']['end_cursor']
+                self.pagination = Continuation(self.pagination.session, self.token, {'shortcode': self.shortcode}, end_cursor, count=50)
+            else:
+                self.pagination = None
 
-        return self._extract_comments(cmt['edges'])
+            return self._extract_comments(cmt['edges'])
+        except Exception as e:
+            print('Broken pagination: ' + str(e), file=sys.stderr)
+            return []
 
 
 class Post(object):
     Media = namedtuple('Media', 'url type id content')
 
-    def __init__(self, edge, index, token):
+    def __init__(self, session, edge, index, token):
         node = edge['node']
         self.index = index
         self.typename = node['__typename']
-        self.comments = Comment(node['edge_media_to_comment'], node['shortcode'], token)
+        self.comments = Comment(session, node['edge_media_to_comment'], node['shortcode'], token)
         self.timestamp = datetime.datetime.fromtimestamp(node['taken_at_timestamp'])
         if self.typename == 'GraphSidecar':  # GraphImage | GraphSidecar | GraphVideo
-            self.media = [self._get_media(edge['node']) for edge in node['edge_sidecar_to_children']['edges']]
+            self.media = [self._get_media(session, edge['node']) for edge in node['edge_sidecar_to_children']['edges']]
         else:
-            self.media = [self._get_media(node)]
+            self.media = [self._get_media(session, node)]
 
         captions = node['edge_media_to_caption']['edges']
         if len(captions) > 0:
@@ -138,15 +152,12 @@ class Post(object):
         else:
             self.caption = None
 
-        keys = ['id', 'shortcode', 'location', '__typename', 'dimensions',
-                'accessibility_caption', 'edge_media_preview_like', 'edge_media_to_tagged_user']
-        new_dict = {k: v for k, v in node.items() if k in keys}
-        self.info = toutf8(json.dumps({**new_dict, 'url': f"https://www.instagram.com/p/{node['shortcode']}"}, indent=2))
+        self.info = toutf8(json.dumps(node, indent=2))
 
     @staticmethod
-    def _get_media(node):
+    def _get_media(session, node):
         url = node['video_url'] if node['is_video'] else node['display_url']
-        return Post.Media(url, node['__typename'], node['id'], DataObject(url))
+        return Post.Media(url, node['__typename'], node['id'], DataObject(session, url))
 
 
 class Profile(object):
@@ -154,19 +165,19 @@ class Profile(object):
         self.token = TokenManager(username)
         self.userinfo = toutf8(json.dumps(self.token.user, indent=2))
 
-        data = Continuation(self.token.profile, {'id': self.token.id}, cursor=None).get()
+        data = Continuation(self.token.session, self.token.profile, {'id': self.token.id}, cursor=None).get()
         media = data['data']['user']['edge_owner_to_timeline_media']
 
         self.count = media['count']
         self.pagination = self._extract_pagination(self.token, media['page_info'])
-        self.posts = [Post(edge, self.count - idx, self.token.comment) for idx, edge in enumerate(media['edges'])]
+        self.posts = [Post(self.token.session, edge, self.count - idx, self.token.comment) for idx, edge in enumerate(media['edges'])]
         self.index = self.count - len(self.posts)
         self.biography = toutf8(self.token.user['biography'])
 
     @staticmethod
     def _extract_pagination(token, page_info):
         if page_info['has_next_page']:
-            return Continuation(token.profile, {'id': token.id}, page_info['end_cursor'])
+            return Continuation(token.session, token.profile, {'id': token.id}, page_info['end_cursor'])
         else:
             return None
 
@@ -177,7 +188,7 @@ class Profile(object):
         data = self.pagination.get()
         media = data['data']['user']['edge_owner_to_timeline_media']
         self.pagination = self._extract_pagination(self.token, media['page_info'])
-        posts = [Post(edge, self.index - idx, self.token.comment) for idx, edge in enumerate(media['edges'])]
+        posts = [Post(self.token.session, edge, self.index - idx, self.token.comment) for idx, edge in enumerate(media['edges'])]
         self.index -= len(posts)
         return posts
 
